e8e2bb6f82b45e86f0a152b350892195c8835cd6
ShuiMuLuo <forevergoodforest@outlook.com>
2025-05-17 23:48:30 +0800

    fix: 修正年龄计算逻辑，确保正确计算感知时间

--- time_compute.py
+++ time_compute.py
@@ -1,21 +1,30 @@
 from math import log
 your_year = input("请输入你的年龄：")
-your_year = int(your_year) - 4
+your_year = int(your_year)
+your_day = your_year * 365
+your_hour = your_day * 24
+your_minute = your_hour * 60
 
-total_years = your_year
-total_days = your_year * 365
-total_hours = total_days * 24
+total_years = your_year - 4
+total_days = (your_year - 4) * 365
+total_hours = (your_year - 4) * 365 * 24
+total_minutes = (your_year - 4) * 365 * 24 * 60
 
 if total_years <= 4:
     mental_years = 1
+    mental_days = 1
+    mental_hours = 1
+    mental_minutes = 1
 else:
     mental_years = 1/log(total_years)
+    mental_days = 1/log(total_days)
+    mental_hours = 1/log(total_hours)
+    mental_minutes = 1/log(total_minutes)
 
-mental_days = 1/log(total_days)
-mental_hours = 1/log(total_hours)
+print("你已经生活了", your_year, "年", "你当前的实际感知的一年时间实际是", mental_years, "年")
+print("你已经生活了", your_day, "天", "你当前的实际感知的一天时间实际是", mental_days, "天")
+print("你已经生活了", your_hour, "小时", "你当前的实际感知的一小时时间实际是", mental_hours, "小时")
+print("你已经生活了", your_minute, "分钟", "你当前的实际感知的一分钟时间实际是", mental_minutes, "分钟")
 
 
 
-print("你现在的对于一年实际感知是", mental_years, "年")
-print("你现在的对于一天的实际感知是", mental_days, "天")
-print("你现在的对于一小时的实际感知是", mental_hours, "小时")
